d79225bc0743292e4e7b6ec4d71e0b0913803209
pulwar <kingmax@inbox.ru>
2024-03-05 22:04:34 +0300

    3 manifest

--- content.js
+++ content.js
@@ -61,16 +61,6 @@ function extractWholeNumber(weightString) {
     return parseInt(parts[0]) + ' ' + parts[1];
 }
 
-// chrome.runtime.sendMessage({
-//     'id': document.querySelector('table.common-grid-table').querySelector('tr').querySelectorAll('td')[1].innerText,
-//     'from': document.querySelector('table.content-padding.table-list').querySelectorAll('tr')[1].querySelectorAll('td')[findCountryCellIndex("Country")].querySelector('div').innerText.split('\n')[0],
-//     'to': addRecipients(),
-//     'name': document.querySelector('table.common-grid-table').querySelectorAll('tr')[1].querySelectorAll('td')[1].innerText,
-//     'weight': extractWholeNumber(extractWeight(document.querySelector('table.common-grid-table').querySelectorAll('tr')[2].querySelectorAll('td')[4].innerText)),
-//     'start': document.querySelector('table.content-padding.table-list').querySelectorAll('tr')[1].querySelectorAll('td')[findCountryCellIndex("Pickup")].querySelector('div').innerText.split('\n')[0],
-//     'end': returnEnd(),
-// });
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("Recv. Send response = " + document.title);
     var data = {
@@ -83,7 +73,5 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         'end': returnEnd(),
     }
     sendResponse(data);
-    //sendResponse({ title: document.title});
-
     return true;
 });
--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,3 @@
-// This callback function is called when the content script has been 
-// injected and returned its results
-function onPageDetailsReceived(pageDetails)  {
-    document.getElementById('from').value = pageDetails.from;
-    document.getElementById('to').value = pageDetails.to;
-    document.getElementById('id').value = pageDetails.id;
-    //document.getElementById('name').value = pageDetails.name;
-    document.getElementById('weight').value = pageDetails.weight;
-    document.getElementById('start').value = pageDetails.start;
-    document.getElementById('end').value = pageDetails.end;
-} 
-
-// Global reference to the status display SPAN
-var statusDisplay = null;
-
 // POST the data to the server using XMLHttpRequest
 function addBookmark() {
     console.log('addBookmark');
@@ -23,18 +8,15 @@ function addBookmark() {
     var from = document.getElementById('from').value;
     var to = document.getElementById('to').value;
     var id = document.getElementById('id').value;
-    //var name = document.getElementById('name').value;
     var weight = document.getElementById('weight').value;
     var start = document.getElementById('start').value;
     var end = document.getElementById('end').value;
-
     var type = document.getElementById('type').value;
     var special = document.getElementById('special').value;
     var ltl = document.getElementById('ltl').value;
     var req = document.getElementById('req').value;
     var comment = document.getElementById('comment').value;
     var company = document.getElementById('company').value;
-
     var fromCode = from.substring(0, 2);
     var toCode = to.substring(0, 2);
 
@@ -51,8 +33,6 @@ function addBookmark() {
     params +=  '/' + comment;
 
     document.getElementById('result').value = params;
-
-
 
     navigator.clipboard.writeText(params)
         .then(() => {
@@ -78,23 +58,13 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 });
 // When the popup HTML has loaded
 window.addEventListener('load', function(evt) {
-
-
     console.log('page loaded');
     $( '.select-field' ).select2( {
         theme: 'bootstrap-5',
         width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
         openOnEnter: false
     } );
-
     document.getElementById('addbookmark').addEventListener('submit', addBookmark);
-
-    // // Get the event page
-    // chrome.runtime.getBackgroundPage(function(eventPage) {
-    //     eventPage.getPageDetails(onPageDetailsReceived);
-    // });
-
-
 });
 
 $(document).ready(function() {
@@ -110,17 +80,6 @@ $(document).ready(function() {
             ltlInput.prop('disabled', false);
             ltlInput.prop('required', true);
         }
-    });
-
-
-    var inputElements = document.querySelectorAll('input.form-control');
-
-    // Add event listener for focus event to each input element
-    inputElements.forEach(function(inputElement) {
-        inputElement.addEventListener('focus', function(event) {
-            // Your event handling code here
-            console.log('Input element focused:', inputElement.id);
-        });
     });
 
 });
